32e332e45714e889e38c5674613430bd6da8112c
曾昭文 <wenzi2020@yeah.net>
2021-03-27 11:24:33 +0800

    commit the JavaScript note.

---
+++ 第02周/day09_array.js
@@ -0,0 +1,72 @@
+/* 定义一个数组，
+记录10个学生姓名，
+并且修改下标为3的学生姓名，
+然后依次打出所有的学生姓名。 */
+var sum = "";
+var student1 = {
+	name: ['某1', '某2', '某3', '某4', '某5', '某6', '某7', '某8', '某9', '某10']
+};
+student1.name[3] = '阿4';
+for (v in student1.name) {
+	sum += student1.name[v] + '\t';
+}
+console.log(sum);
+// join('');unshift('');shift();push('');pop();splice(0,0,'');
+/* 定义一个数组，来统计10个学生的分数，
+获取数组中学生分数不及格的人数，
+获取数组中学生分数大于80的人数。 */
+var student2 = {
+	score: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+};
+var s1 = [],
+	s2 = new Array();
+for (v in student2.score) {
+	var a = student2.score[v];
+	a < 60 ? s1.push(a) : (a > 80 ? s2.unshift(a) : '');
+}
+console.log(s1.length, s2.length);
+// 把下标为2-5的学生的分数加10分。
+for (v in student2.score) {
+	var a = student2.score[v];
+	v >= 2 && v <= 5 ? student2.score[v] = a + 10 : '';
+}
+console.log(student2.score.join(' '));
+// concat('['']');sort();reserve();slice(0,0);(last)indexOf('',0);
+// 定义一个数组[10,8,12,5,20,30,15]
+/* 1.将数组的所有的元素输出在页面。
+2.修改下标为2的元素改为35并且将元素的内容输出页面。
+3.在页面编写一个按钮,点击该按钮,然后弹出数组中所有的元素的和。
+4.在页面编写一个按钮,点击该按钮,然后弹出数组中所有的元素的平均值。
+5.在页面编写一个按钮,点击按钮,弹出数组中最大的数。 */
+var arr1 = [10, 8, 12, 5, 20, 30, 15];
+console.log(arr1.join(' '));
+arr1.splice(2, 1, 35);
+console.log(arr1.join(' '));
+var xSum = 0;
+var avgXSum = 0;
+var maxXSum = arr1[0];
+var el = document.createElement('button');
+var te = document.createTextNode('弹窗按钮');
+el.appendChild(te);
+document.body.appendChild(el).onclick = () => {
+	arr1.forEach((v, k, arr) => {
+		xSum += v;
+		avgXSum += v / arr.length;
+		maxXSum < v ? maxXSum = v : '';
+	});
+	window.alert([xSum, avgXSum, maxXSum]);
+}
+// 数组为：[1，3，5，7，24，32，25，48]。
+/* 输出数组中的偶数，
+求数组中奇数的个数和偶数的个数，
+求上述数组的平均值。 */
+var even = [];
+var odd = [];
+var evenSum = 0;
+var oddSum = 0;
+var arr2 = [1, 3, 5, 7, 24, 32, 25, 48];
+arr2.forEach((v, k, arr) => {
+	v % 2 == 0 ? even.push(v) : odd.push(v);
+	v % 2 == 0 ? evenSum += v : oddSum += v;
+});
+console.log(even.join(' '), even.length, odd.length, evenSum / even.length, oddSum / odd.length);
---
+++ 第02周/day10_object.js
@@ -0,0 +1,101 @@
+// 函数的递归用法 return 1 终止
+var func = (num) => {
+	if (num == 1) {
+		return 1;
+	}
+	return func(num - 1) * num;
+}
+console.log(func(10));
+// 1. 比较排序： 选择排序(先前再后)
+var a = [5, 1, 3, 2, 4, 9, 6, 8, 7];
+for (var i = 0; i < a.length - 1; i++) {
+	for (var u = i + 1; u < a.length; u++) {
+		if (a[i] > a[u]) {
+			var temp = a[i];
+			a[i] = a[u];
+			a[u] = temp;
+		}
+	}
+}
+console.log(a.join(' '));
+// 2. 比较排序： 冒泡排序(先后再前)
+var b = [5, 1, 3, 2, 4, 9, 6, 8, 7];
+for (var i = 0; i < b.length - 1; i++) {
+	for (var u = 0; u < b.length - (i + 1); u++) {
+		if (b[u] > b[u + 1]) {
+			var temp = b[u];
+			b[u] = b[u + 1];
+			b[u + 1] = temp;
+		}
+	}
+}
+console.log(b.join(' '));
+// 数组Array
+// new Array('值')|['值']
+// 对象Object
+// new Object(键:'值'|f) | {键:'值'|f}
+/* 创建一个对象通过三种方式，
+给对象设置属性name, age, sex, tel的值，
+然后修改name和sex然后输出在控制台。 */
+var ObjName = function(name, sex, age, tel) {
+	this.name = name;
+	this.sex = sex;
+	this.age = age;
+	this.tel = tel;
+}
+var objs = new ObjName('某', '保密', 18, '020-0');
+objs['name'] = '阿';
+objs.sex = '未知';
+for (i in objs) {
+	console.log(objs[i]);
+}
+// 定义两个数组(全是数组)
+var arr1 = [1.5, 2.5, 3.5],
+	arr2 = [4.5, 5.5, 6.5];
+var max1 = arr1[0],
+	max2 = arr2[0];
+var avg1 = 0,
+	avg2 = 0;
+/* 1. 求出两个数组的最大值，
+然后把这两个最大值一起通过Math类进行比较求出两个最大值之间的最大值。 */
+arr1.forEach((v, k, arr) => {
+	if (max1 < v) {
+		max1 = v;
+	}
+	avg1 += v / arr.length;
+});
+arr2.forEach((v, k, arr) => {
+	if (max2 < v) {
+		max2 = v;
+	}
+	avg2 += v / arr.length;
+});
+console.log(Math.max(max1, max2));
+/* 2. 请求两个数组的平均值， 
+然后通过Math类对两个数组的平均值进行四舍五入， 
+还有向上舍入和向下舍入。 */
+var avg = [avg1, avg2];
+avg.forEach((v, k, arr) => {
+	console.log(Math.round(v), Math.ceil(v), Math.floor(v));
+});
+/* 3. 通过Math类生成随机数添加到两个数组中。 */
+arr1.push(Math.random());
+arr2.push(Math.random());
+console.log(arr1.join(' '), arr2.join(' '));
+/* 创建一个时间类，
+获取当前时间的在一个月中的天数，
+在一周内的星期数，
+获取当前时间的年份，
+获取当前时间的毫秒数，
+然后获取当前时间按照中国的年份/月/日输出 时分秒。 */
+var date = new Date();
+console.log(date.getDate(), date.getDay(), date.getFullYear(), date.getMilliseconds());
+console.log(date.toLocaleString());
+// 答案：30
+var a;
+var b = a - 0;
+if (b == b) {
+	console.log(b * 2 + "2" - 0 + 4);
+} else {
+	console.log(!b * 2 + "2" - 0 + 8);
+}
