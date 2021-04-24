920ce062fa150f6efa03e0052e8c129814d6cdd6
曾昭文 <wenzi2020@yeah.net>
2021-04-24 16:14:35 +0800

    Create day28_get.php

---
+++ 第06周/day28_get.php
@@ -0,0 +1,17 @@
+<?php
+include_once('./config.php');
+$res=@mysqli_query($con, "select * from comment");
+if (!$res) {
+    echo "<script>alert('查询记录失败')</script>";
+    exit();
+}
+$data=[];
+while ($row=mysqli_fetch_assoc($res)) {
+    $data[]=$row;
+}
+if (mysqli_num_rows($res)>0) {
+    echo Api::json(200, 'success', $data);
+} else {
+    echo Api::json(400, 'error');
+}
+mysqli_close($con);
