6b13b9b83b7e1a1ddbf423918e3518a44220cb7a
曾昭文 <wenzi2020@yeah.net>
2021-04-24 16:13:48 +0800

    Create day28_config.php

---
+++ 第06周/day28_config.php
@@ -0,0 +1,15 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+$con=@mysqli_connect('127.0.0.1', 'root', 'root', 'gdmecmallsys', 3306);
+if (!$con) {
+    echo "<script>alert('500:数据库连接失败')</script>";
+    exit();
+}
+mysqli_query($con, 'set names utf8');
+class Api
+{
+    public static function json($code, $msg, $data=array())
+    {
+        return json_encode(array('code'=>$code,'msg'=>$msg,'data'=>$data));
+    }
+}
